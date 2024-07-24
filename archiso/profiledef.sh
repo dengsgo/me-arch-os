9886c8c5b9002626df34ffceb693f1229bcc82bb
Deng.Liu <dengsgo@yoytang.com>
2024-07-24 22:04:08 +0800

    Update profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m%d)"
+iso_label="MEARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m%d)"
 iso_publisher="MeArch <https://www.yoytang.com>"
 iso_application="MeArch Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d-%H)"
@@ -19,6 +19,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
+
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/root/.config"]="0:0:700"
