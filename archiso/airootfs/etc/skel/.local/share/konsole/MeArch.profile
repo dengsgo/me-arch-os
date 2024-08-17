daf96c86bd9745e5a7054f430485af210427dbf1
dengsgo <dengsgo@yoytang.com>
2024-08-17 20:54:23 +0800

    add config konsole breeze

---
+++ archiso/airootfs/etc/skel/.local/share/konsole/MeArch.profile
@@ -0,0 +1,9 @@
+[Appearance]
+ColorScheme=MeArch
+Font=Hack,10,-1,7,400,0,0,0,0,0,0,0,0,0,0,1
+
+[General]
+Name=MeArch
+Parent=FALLBACK/
+TerminalColumns=110
+TerminalRows=36
---
+++ archiso/airootfs/root/theme-prepare.sh
@@ -0,0 +1,5 @@
+# Cursor Icon
+wget Bibata-Modern-Ice.tar.xz https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
+tar -Jxf Bibata-Modern-Ice.tar.xz
+mkdir -p /archiso/airootfs/usr/share/icons
+cp -rf Bibata-Modern-Ice /archiso/airootfs/usr/share/icons
