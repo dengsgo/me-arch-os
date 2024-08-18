5cb058cae976d0e8b08376cd6be7da916c99d232
dengsgo <dengsgo@yoytang.com>
2024-08-18 14:57:54 +0800

    update sh

--- archiso/airootfs/root/theme-prepare.sh
+++ archiso/airootfs/root/theme-prepare.sh
@@ -3,3 +3,6 @@ wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Mod
 tar -Jxf Bibata-Modern-Ice.tar.xz
 mkdir -p /archiso/airootfs/usr/share/icons
 cp -rf Bibata-Modern-Ice /archiso/airootfs/usr/share/icons/
+ls -al /archiso/airootfs/usr/share/icons/
+echo "Cursor Icon Path: /archiso/airootfs/usr/share/icons/Bibata-Modern-Ice"
+ls -al /archiso/airootfs/usr/share/icons/Bibata-Modern-Ice
