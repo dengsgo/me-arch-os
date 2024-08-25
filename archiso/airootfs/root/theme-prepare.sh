78437aba006b7bfad7e745d55bab0f9bb518d491
dengsgo <dengsgo@yoytang.com>
2024-08-25 13:07:33 +0800

    fix

--- archiso/airootfs/root/theme-prepare.sh
+++ archiso/airootfs/root/theme-prepare.sh
@@ -11,6 +11,6 @@ ls -al archiso/airootfs/etc/skel/.local/share/icons/Bibata-Modern-Ice
 # Icon
 wget -O Qogir.tar.xz https://sourceforge.net/projects/mearch/files/theme/Icons-01-Qogir-2024-07-29.tar.xz/download
 tar -Jxf Qogir.tar.xz
-cp -rf Qogir/* archiso/airootfs/etc/skel/.local/share/icons/
+cp -rf Qogir archiso/airootfs/etc/skel/.local/share/icons/
 ls -al archiso/airootfs/etc/skel/.local/share/icons/
 echo "Icons Path: archiso/airootfs/etc/skel/.local/share/icons/"
