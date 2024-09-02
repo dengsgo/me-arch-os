740c0e9bdda5a3c92580f0db2a0196b8df1aee8b
Deng.Liu <dengsgo@yoytang.com>
2024-09-02 11:06:41 +0000

    add local repo

--- archiso/airootfs/root/theme-prepare.sh
+++ archiso/airootfs/root/theme-prepare.sh
@@ -14,3 +14,9 @@ tar -Jxf Qogir.tar.xz
 cp -rf Qogir archiso/airootfs/etc/skel/.local/share/icons/
 ls -al archiso/airootfs/etc/skel/.local/share/icons/
 echo "Icons Path: archiso/airootfs/etc/skel/.local/share/icons/"
+
+# local repo
+mkdir -p /mearch-os/repo
+wget -O mearch-os-repo.tar.gz https://sourceforge.net/projects/mearch/files/package/mearch-os-repo.tar.gz/download
+tar -zxvf mearch-os-repo.tar.gz
+cp -rf x86_64 /mearch-os/repo/
